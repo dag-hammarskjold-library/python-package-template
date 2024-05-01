cd91c3cffdad9b8c0d7fbed1f6abb52997e3d1f6
jbukhari <joe.bukhari@gmail.com>
2024-05-01 13:59:24 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+NAME = ''
+DESCRIPTION = ''
+VERSION = ''
+
+with open("README.md") as f:
+    long_description = f.read()
+    
+with open("requirements.txt") as f:
+    requirements = list(filter(None,f.read().split('\n')))
+
+setup(
+    name = NAME,
+    version = VERSION,
+    #url = '',
+    author = 'United Nations Dag Hammarskjöld Library',
+    author_email = 'library-ny@un.org',
+    license = 'http://www.opensource.org/licenses/bsd-license.php',
+    packages = find_packages(exclude=['test']),
+    test_suite = 'tests',
+    install_requires = requirements,
+    description = DESCRIPTION,
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    python_requires = '>=3.8',
+    entry_points = {
+        # see https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html#the-console-scripts-entry-point
+        'console_scripts': []
+    }
+)
+
